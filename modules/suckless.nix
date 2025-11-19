104e46427c2ac6d0e26b92eaf51feb0aae46d096
Álvaro Candela <alvarocandela47@gmail.com>
2025-11-19 16:01:05 +0100

    Updated dwm sha256 value

--- modules/suckless.nix
+++ modules/suckless.nix
@@ -8,7 +8,7 @@
           owner = "alvarocandela";
           repo = "dwm";
           rev = "main";
-          sha256 = "sha256-2sF7xKcw3UGMtDKzIB/VUhbTqR80U8xjreA9kdSlvrQ="; # CHANGE THIS
+          sha256 = "sha256-5LbowyinyeQdnwP5O6JQUDVAWgV78j/3IJJvTLgu+3k="; # CHANGE THIS
         };
       });
 

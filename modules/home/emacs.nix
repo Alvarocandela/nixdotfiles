ae1088b97816c443898d1303fabd5eb6f075a9aa
Álvaro Candela <alvarocandela47@gmail.com>
2025-11-26 11:27:58 +0100

    Added emacs config

---
+++ modules/home/emacs.nix
@@ -0,0 +1,12 @@
+{ config, pkgs, ... }:
+
+{
+  nixpkgs.overlays = [
+  (import (builtins.fetchTarball https://github.com/nix-community/emacs-overlay/archive/master.tar.gz))
+];
+
+environment.systemPackages = [
+  pkgs.emacsGcc  # Installs Emacs 28 + native-comp
+  clang
+];
+}
